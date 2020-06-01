16c254ec77f15f5566743f782757160182df00b2
Alexandr Sashchenko <santys87@gmail.com>
2020-06-01 15:34:18 +0300

    Added angular app (cooking-ng-app) to current repository; Added empty components for main functionality; Set up basic routing; Added simple login form;

--- cooking-ng-app/src/app/app.module.ts
+++ cooking-ng-app/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { IngredientsComponent } from './ingredients/ingredients.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    IngredientsComponent,
+    RecipesComponent,
+    MyProfileComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
